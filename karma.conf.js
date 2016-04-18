45e097acf63f7163a7b17d9a8e58e5c32936807c
PawelGutkowski <pawel.gutkowski.1993@gmail.com>
2016-04-18 10:41:52 +0200

    RA-1106/1107/1108 travis configuration

--- karma.conf.js
+++ karma.conf.js
@@ -85,6 +85,10 @@ module.exports = function (config) {
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,    
   }
+
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
   
   config.set(configuration);
 };
