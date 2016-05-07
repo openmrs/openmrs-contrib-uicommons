75626d87d7fe1e972ccd4a119b2873fb414a4aa7
Paweł Gutkowski <pawel.gutkowski.1993@gmail.com>
2016-05-07 17:25:17 +0200

    Update openmrs-contrib-uicommons.js

--- openmrs-contrib-uicommons.js
+++ openmrs-contrib-uicommons.js
@@ -1,16 +1,16 @@
-import stylesheet from "./src/scss/sass/openmrs-refapp.scss";
+import openmrsUicommonsStylesheet from "./src/scss/sass/openmrs-refapp.scss";
 
-import header from './angular/openmrs-header/openmrs-header.component.js';
-import breadcrumbs from './angular/openmrs-breadcrumbs/openmrs-breadcrumbs.component.js';
-import conceptAutocomplete from './angular/openmrs-conceptAutocomplete/openmrs-conceptAutocomplete.component.js';
-import openmrsRest from './angular/openmrs-rest/openmrs-rest.js';
-import openmrsList from './angular/openmrs-list/openmrs-list.component.js';
-import openmrsSearch from './angular/openmrs-search/openmrs-search.component.js';
-import deleteAlert from './angular/openmrs-deleteAlert/delete-alert.component.js';
-import retireAlert from './angular/openmrs-retireAlert/retire-alert.component';
+import openmrsUicommonsHeader from './angular/openmrs-header/openmrs-header.component.js';
+import openmrUicommonsBreadcrumbs from './angular/openmrs-breadcrumbs/openmrs-breadcrumbs.component.js';
+import openmrsUicommonsConceptAutocomplete from './angular/openmrs-conceptAutocomplete/openmrs-conceptAutocomplete.component.js';
+import openmrUicommonsRest from './angular/openmrs-rest/openmrs-rest.js';
+import openmrsUicommonsList from './angular/openmrs-list/openmrs-list.component.js';
+import openmrsUicommonsSearch from './angular/openmrs-search/openmrs-search.component.js';
+import openmrsUicommonsDeleteAlert from './angular/openmrs-deleteAlert/delete-alert.component.js';
+import openmrsUicommonsRetireAlert from './angular/openmrs-retireAlert/retire-alert.component';
 
 
-var lib = angular.module('openmrs-contrib-uicommons',
+export default angular.module('openmrs-contrib-uicommons',
 				[
 					'openmrs-contrib-uicommons.header',
 					'openmrs-contrib-uicommons.breadcrumbs', 
@@ -21,16 +21,3 @@ var lib = angular.module('openmrs-contrib-uicommons',
 					'openmrs-contrib-uicommons.list',
 					'openmrs-contrib-uicommons.search'
                 ]);
-
-module.exports = {
-	stylesheet : stylesheet,
-	header : header,
-	breadcrumbs : breadcrumbs,
-	conceptautocomplete : conceptAutocomplete,
-	openmrsRest : openmrsRest,
-	openmrsList : openmrsList,
-	openmrsSearch : openmrsSearch,
-	deleteAlert : deleteAlert,
-	retireAlert : retireAlert,
-	lib : lib
-};
