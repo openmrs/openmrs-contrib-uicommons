1478657e2a685e6bd597a5e885dfa021b0ad377b
Tomasz Marzeion <marzeion.tomasz@gmail.com>
2016-07-19 14:46:24 +0200

    Filter by class/source fix

--- angular/openmrs-list/openmrs-list.component.js
+++ angular/openmrs-list/openmrs-list.component.js
@@ -54,13 +54,13 @@ function openmrsList(openmrsRest, openmrsNotification, $scope, $location) {
     vm.isAdvancedSearchPossible = isAdvancedSearchPossible;
     vm.advancedSearchResource = resolveAdvancedSearchResource();
     vm.isAdvancedSearchModeOn = isAdvancedSearchModeOn;
-    vm.selectedAdvancedSearchDataUuid = '';
+    vm.selectedAdvancedSearchDataUuid;
     vm.advancedSearchResourceLabel = '';
 
     vm.advancedSearchData = [{}];
 
     function isAdvancedSearchModeOn() {
-        return vm.advancedSearchData != null && vm.selectedAdvancedSearchDataUuid.length > 0
+        return vm.advancedSearchData != null && angular.isDefined(vm.selectedAdvancedSearchDataUuid);
     }
 
     function isAdvancedSearchPossible() {
